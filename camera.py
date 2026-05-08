4782c4e6f159f8efdcd71dfea5b41b0dc2f53690
lucasnguyenTK <lucsanguyenqu06@gmail.com>
2026-05-07 21:07:38 -0700

    resizing cam window

--- camera.py
+++ camera.py
@@ -3,6 +3,10 @@ import numpy as np
 
 #default camera index 0
 cam = cv.VideoCapture(0)
+#parameters for camera window dimension
+width = 1280
+height = 960
+dim = (width, height)
 
 if not cam.isOpened():
     print("Cannot open camera")
@@ -11,7 +15,8 @@ if not cam.isOpened():
 while True:
     #capture frame-by-frame
     ret, frame = cam.read()
-
+    #resize camera display window
+    frame = cv.resize(frame, dim, interpolation=cv.INTER_AREA)
     #if frame is read correctly, ret = True
     if not ret:
         print("Can't read frame. Exit")
@@ -23,7 +28,6 @@ while True:
     #press 'q' to exit loop = stop displaying camera
     if cv.waitKey(1) == ord('q'):
         break
-
 #release capture 
 cam.release()
 cv.destroyAllWindows()
